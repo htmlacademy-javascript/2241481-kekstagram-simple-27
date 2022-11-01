b7bf580b61a6ec65b67c65c41df7477809145464
Mr Muscl <anton.kurkov@gmail.com>
2022-11-01 20:35:47 +0300

    Fixing remarks for previous homework.

--- js/form.js
+++ js/form.js
@@ -1,18 +1,30 @@
-const body = document.querySelector('body');
+const body = document.body;
 const editImageForm = document.querySelector('.img-upload__overlay');
 const imgUploader = document.querySelector('#upload-file');
+const closeImgEditBtn = document.querySelector('#upload-cancel');
 
-const showEditImageForm = ()=>{
-  body.classList.add('modal-open');
-  editImageForm.classList.remove('hidden');
-};
-
-const hideEditImageForm = ()=>{
+const hideEditImageForm = () =>{
   body.classList.remove('modal-open');
   editImageForm.classList.add('hidden');
   imgUploader.value = '';
+  document.removeEventListener('keydown', keyDownHandler);
 };
 
-export {
-  showEditImageForm,
-  hideEditImageForm};
+const keyDownHandler = (evt) =>{
+  if (evt.key === 'Escape'){
+    hideEditImageForm();
+  }
+};
+
+const showEditImageForm = () =>{
+  body.classList.add('modal-open');
+  editImageForm.classList.remove('hidden');
+  document.addEventListener('keydown', keyDownHandler);
+};
+
+const initImageForm = () =>{
+  imgUploader.addEventListener('change', showEditImageForm);
+  closeImgEditBtn.addEventListener('click', hideEditImageForm);
+};
+
+export {initImageForm};
--- js/main.js
+++ js/main.js
@@ -1,17 +1,7 @@
 import {generatePosts} from './data.js';
 import {createThumbnailsLayout} from './layout.js';
-import { hideEditImageForm, showEditImageForm } from './form.js';
+import {initImageForm} from './form.js';
 
 const posts = generatePosts();
 createThumbnailsLayout(posts);
-
-const imgUploader = document.querySelector('#upload-file');
-imgUploader.addEventListener('change', showEditImageForm);
-
-const closeImgEditBtn = document.querySelector('#upload-cancel');
-closeImgEditBtn.addEventListener('click', hideEditImageForm);
-document.addEventListener('keydown', (evt)=>{
-  if (evt.key === 'Escape'){
-    hideEditImageForm();
-  }
-});
+initImageForm();
